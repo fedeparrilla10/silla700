8d4b2320ee1db0528396e665ddf383ee60d2f93a
fedeparrilla10 <federicoivanpd@gmail.com>
2024-01-24 21:52:13 +0200

    first commit

---
+++ src/data/first_section.js
@@ -0,0 +1,52 @@
+export const firstSectionData = [
+  {
+    id: 1,
+    img: '/silla675/sillacolor.png',
+    title: '',
+    icon: '/silla675/ps5.svg',
+    extraIcons: [
+      {
+        icon: '/silla675/ps4.svg',
+        class: 'w-8 h-8',
+      },
+      {
+        icon: '/silla675/switch.svg',
+        class: 'w-8 h-8',
+      },
+      {
+        icon: '/silla675/pc.svg',
+        class: 'w-8 h-8',
+      },
+      {
+        icon: '/silla675/mobile.svg',
+        class: 'w-8 h-8',
+      },
+    ],
+    class: 'w-16 h-16',
+  },
+  {
+    id: 2,
+    img: '/silla675/sillalado2.png',
+    title: '',
+    icon: '/silla675/ps5.svg',
+    extraIcons: [
+      {
+        icon: '/silla675/ps4.svg',
+        class: 'w-8 h-8',
+      },
+      {
+        icon: '/silla675/switch.svg',
+        class: 'w-8 h-8',
+      },
+      {
+        icon: '/silla675/pc.svg',
+        class: 'w-8 h-8',
+      },
+      {
+        icon: '/silla675/mobile.svg',
+        class: 'w-8 h-8',
+      },
+    ],
+    class: 'w-16 h-16',
+  },
+];
---
+++ src/data/specifications.js
@@ -0,0 +1,88 @@
+export const specsImgs = [
+  {
+    id: 1,
+    name: 'Mando RGB',
+    subtitle: 'Inalámbrico',
+    img: '/silla675/remote.svg',
+  },
+  {
+    id: 2,
+    name: 'Almohadilla',
+    subtitle: 'Adaptable',
+    img: '/silla675/comfort.svg',
+  },
+];
+
+export const firstTable = [
+  {
+    title: 'Tipo de dispositivo',
+    description: 'Silla gaming',
+  },
+  {
+    title: 'Anchura',
+    description: '53cm',
+  },
+  {
+    title: 'Altura',
+    description: '132cm',
+  },
+  {
+    title: 'Profundidad',
+    description: '53cm',
+  },
+  {
+    title: 'Color (por fabricante)',
+    description: 'Gris y multicolor',
+  },
+  {
+    title: 'Peso',
+    description: '23kg',
+  },
+  {
+    title: 'Ruedas',
+    description: '5',
+  },
+  {
+    title: 'Contenido',
+    description: 'x1 Silla Gaming',
+  },
+  {
+    title: 'Diámetro de transductores',
+    description: '40mm',
+  },
+];
+
+export const secondTable = [
+  {
+    title: 'Inalámbrico',
+    description: 'Sí',
+  },
+  {
+    title: 'Compatibilidad HS',
+    description: 'PS5/PS4/Switch/PC/Tablet/Mobile',
+  },
+  {
+    title: 'Compatibilidad HX',
+    description: 'XBOX X-S/XBOX One/PC/Mobile',
+  },
+  {
+    title: 'Frecuencia de respuesta del micrófono',
+    description: '100 Hz - 10 kHz',
+  },
+  {
+    title: 'Micrófono plegable',
+    description: 'Sí',
+  },
+  {
+    title: 'Tipo de conexión',
+    description: 'Dongle USB C & Bluetooth',
+  },
+  {
+    title: 'Control de volumen',
+    description: 'Rueda de volumen en la oreja',
+  },
+  {
+    title: 'Duración de la batería',
+    description: '24h / 18h',
+  },
+];
