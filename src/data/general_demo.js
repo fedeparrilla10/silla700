088723f5ac677be03ac96f15f013717f8fb4b6ca
fedeparrilla10 <federicoivanpd@gmail.com>
2024-01-25 20:48:35 +0200

    [fix] videos not showing

--- src/data/general_demo.js
+++ src/data/general_demo.js
@@ -1,17 +1,17 @@
 export const generalDemoData = [
   {
     id: 1,
-    video: 'silla700/inclinacion.mp4',
+    video: '/silla700/inclinacion.mp4',
     class: 'w-16 h-16 pl-5',
   },
   {
     id: 2,
-    video: 'silla700/elevacion.mp4',
+    video: '/silla700/elevacion.mp4',
     class: 'w-16 h-16',
   },
   {
     id: 3,
-    video: 'silla700/reclinacion.mp4',
+    video: '/silla700/reclinacion.mp4',
     class: 'w-16 h-16',
   },
 ];
